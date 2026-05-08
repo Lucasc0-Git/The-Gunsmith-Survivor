583c41ad1393a9bf579a065efbaff0a422b0151e
Lucasc0-Git <lucasc0.yt@gmail.com>
2026-05-08 11:00:47 +0200

    Added tutorial

--- Godot-Project-Files/Scripts/Main/start_scene.gd
+++ Godot-Project-Files/Scripts/Main/start_scene.gd
@@ -3,29 +3,35 @@ extends Control
 @onready var new_game: Button = $VBoxContainer/NewGameButton
 @onready var options: Button = $VBoxContainer/OptionsButton
 @onready var StartScene : PackedScene = preload("res://Scenes/StartScene.tscn")
+@onready var tutorial_panel: Control = $Control
 @export var MainScene : PackedScene
 @export var OptScene : PackedScene
 
 var world_seed := randi()
 
 func _ready() -> void:
+	tutorial_panel.visible = false
 	GameManager.current_world_seed = world_seed
 
 func _on_new_game_button_pressed() -> void:
 	AudioManager.play_button_click()
 	get_tree().change_scene_to_packed(MainScene)
 
-
 func _on_options_button_pressed() -> void:
 	AudioManager.play_button_click()
 	get_tree().change_scene_to_packed(OptScene)
-
 
 func _on_quit_button_pressed() -> void:
 	Settings.save()
 	get_tree().quit()
 
-
 func _on_saves_button_pressed() -> void:
 	AudioManager.play_button_click()
 	get_tree().change_scene_to_file("res://Scenes/SavesScene.tscn")
+
+func _on_tutorial_button_pressed() -> void:
+	AudioManager.play_button_click()
+	tutorial_panel.visible = true
+func _on_back_button_pressed() -> void:
+	AudioManager.play_button_click()
+	tutorial_panel.visible = false
